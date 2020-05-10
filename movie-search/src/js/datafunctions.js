88006d87051352aa81b532fdd755edd4fd09517e
IlliaKhvalko <khvalko.ilya@gmail.com>
2020-05-10 22:22:07 +0300

    feat: add jest tests

--- movie-search/src/js/datafunctions.js
+++ movie-search/src/js/datafunctions.js
@@ -2,6 +2,7 @@ import create from './utils/create.js';
 import CreateInfo from './utils/createInfo.js';
 import createSwiper from './utils/swiper-module.js';
 import vars from './variables.js';
+import {checkRus, getTranslation} from './russian.js'
 
 const observer = new MutationObserver(() => {
   if (vars.next.getAttribute('aria-disabled') === 'true' && vars.accessUpdate) {
@@ -14,26 +15,11 @@ observer.observe(vars.next, {
   attributes: true,
 });
 
-function checkRus() {
-  const matches = vars.word.match(/[а-я\s]/gi);
-  if (matches !== null && matches.length === vars.word.length) {
-    return true;
-  }
-  return false;
-}
-
-async function getTranslation() {
-  const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200507T161021Z.e1df929ffbdcfb02.e69f728503d882be70c316af4862d16dab9795de&text=${vars.word}&lang=ru-en`;
-  const res = await fetch(url);
-  const data = await res.json();
-  return data.text[0];
-}
 
 function changeModeForLoading(access, toggleType) {
   vars.accessUpdate = access;
   vars.loading.classList[toggleType]('none');
 }
-
 
 async function getMovieData() {
   changeModeForLoading(false, 'remove');
